f5009e6249a858e0b11c8dc5f81a7b27cf7a56c9
[React-Hooks] <[ruslanprivozin277@gmail.com]>
2023-06-24 00:22:52 +0300

    user Card

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import UserCard from "../components/userCard";
 
 const MainLayout = () => {
-  return <h2>Main Layout</h2>;
+  return (
+    <>
+      <h2>Main Layout</h2>
+      <UserCard
+        label={"Привозин Руслан"}
+        age={35}
+        src={"https://avatars.githubusercontent.com/u/95099849?v=4"}
+        desk={"Не ебите мозги "}
+      />
+    </>
+  );
 };
 
 export default MainLayout;
