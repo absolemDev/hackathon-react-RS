ae8e80355e2a8b1e914a69075201dc1d918e6017
Ilya Frolov <absolem.dev@gmail.com>
2023-06-23 19:23:00 +0400

    add main page template

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <>
-      <h2>Main Layout</h2>
-    </>
+    <div className="d-flex flex-column h-100">
+      <header className="py-3">
+        <h1 className="text-center text-light">Hackathon React</h1>
+        <h2 className="text-center text-light">
+          Junior Fronted Developer. 8 поток. 82 группа. Team 1
+        </h2>
+      </header>
+      <main className="bg-dark flex-fill">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,8 +1,13 @@
 import { Navigate } from "react-router-dom";
 import MainLayout from "./layouts/main";
+import MainPage from "./components/page/mainPage";
 
 const routes = (isLoggedIn, location) => [
-  { path: "", element: <MainLayout /> },
+  {
+    path: "",
+    element: <MainLayout />,
+    children: [{ path: "", element: <MainPage /> }]
+  },
   {
     path: "*",
     element: <Navigate to="/" />
