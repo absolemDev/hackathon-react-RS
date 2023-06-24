5f2c23f82691e9b953c9bca3954b798df3dad017
Saparow Maksat <makezovs@gmail.com>
2023-06-24 21:05:16 +0300

    fix userPage add onClick Back

--- src/app/components/page/userPage.jsx
+++ src/app/components/page/userPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getTeamLoadingStatus, getTeamMember } from "../../store/team";
 import ProgressBar from "../common/progressBar";
 
 function UserPage() {
+  const navigate = useNavigate();
   const { nick } = useParams();
   const member = useSelector(getTeamMember(nick));
   const isLoading = useSelector(getTeamLoadingStatus());
+  const handleClick = () => {
+    navigate(-1);
+  };
 
   if (isLoading) return "Loading...";
 
   return (
     <div className="container text-center text-light">
       <div className="row pt-3">
-        <div className="col-4">
+        <div className="col-md-4">
           <img
             style={{
               objectFit: "cover",
@@ -23,18 +27,12 @@ function UserPage() {
               borderRadius: "5%"
             }}
             src={member.photo}
+            onClick={handleClick}
             alt="..."
           />
         </div>
         <div className="col">
-          <div
-            className="text-end"
-            style={{
-              objectFit: "cover",
-              width: "100%",
-              height: "500px"
-            }}
-          >
+          <div className="text-end">
             <div className="fs-1">{member.name}</div>
             <div className="fs-5">{`${member.age} лет`}</div>
             <div className="fs-3 m-2">{member.about}</div>
