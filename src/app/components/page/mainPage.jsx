232a8c15145c472f35de9e123ffe569e959fe1cf
Vladislav <vlad.argus@gmail.com>
2023-06-24 15:52:29 +0300

    Implemented favorites page

--- src/app/components/page/mainPage.jsx
+++ src/app/components/page/mainPage.jsx
@@ -3,17 +3,34 @@ import Slider from "../common/slider";
 import { aboutTeam } from "../../content/aboutTeam";
 import UserCard from "../ui/userCard";
 import SocialLink from "../common/socialLink";
+import { useSelector } from "react-redux";
+import { getTeam } from "../../store/team";
+import Btn from "../common/btn";
+import localStorageService from "../../services/localStorage.service";
 
 const MainPage = () => {
+  const team = useSelector(getTeam());
+  if (!team) return "Loading...";
+
   return (
     <>
       <Slider sliders={aboutTeam} />
-      <UserCard
-        label={"Привозин Руслан"}
-        age={35}
-        src={"https://avatars.githubusercontent.com/u/95099849?v=4"}
-        desk={"Люблю кодить"}
-      />
+      <ul className="cards-container">
+        {team.map((member) => (
+          <li key={member.nick} className="m-4">
+            <UserCard label={member.name} age={member.age} src={member.photo} />
+
+            {/* Кнопка добавления в избранное для тестирования */}
+            <div className="mt-3 d-flex justify-content-center">
+              <Btn
+                color="primary"
+                onClick={() => localStorageService.setFavorite(member.nick)}
+                label="В избранные"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <p className="text-light mt-5">Пример использования SocialLink</p>
       <div className="my-3 w-25 d-flex justify-content-around align-items-center">
--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { getAge } from "../../utils/getAge";
+import Btn from "../common/btn";
 
-const UserCard = ({ label, age, src, desk }) => {
+const UserCard = ({ label, age, src, desc }) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img src={src} className="card-img-top" alt="user image" />
+      <img
+        src={src}
+        className="card-img-top"
+        style={{
+          objectPosition: "0 0",
+          objectFit: "cover",
+          width: "100%",
+          height: "300px"
+        }}
+        alt="user image"
+      />
       <div className="card-body">
         <h5 className="card-title">{label}</h5>
-        <h6 className="card-title">{`Возраст: ${age} лет`}</h6>
-        <div className="card-text">{desk}</div>
+        <h6 className="card-title">{`Возраст: ${getAge(age)}`}</h6>
+        <div className="mt-3">
+          <Btn
+            color="dark"
+            onClick={() => console.log("link")}
+            label="Подробнее..."
+          />
+        </div>
       </div>
-
-      <div className="card-body"></div>
     </div>
   );
 };
@@ -20,7 +36,7 @@ UserCard.propTypes = {
   label: PropTypes.string,
   age: PropTypes.number,
   src: PropTypes.string,
-  desk: PropTypes.string
+  desc: PropTypes.string
 };
 
 export default UserCard;
