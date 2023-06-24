a667209ec4d9139ddd8e1bc7bfbad1ac7c3567a2
Ilya Frolov <absolem.dev@gmail.com>
2023-06-24 09:12:30 +0400

    add slider component

---
+++ src/app/content/aboutTeam.js
@@ -0,0 +1,17 @@
+export const aboutTeam = [
+  {
+    title: "First slide label",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam reprehenderit, nostrum deserunt magnam facere illum reiciendis suscipit ad nisi dicta totam. Laboriosam facere quasi assumenda."
+  },
+  {
+    title: "First slide label",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam reprehenderit, nostrum deserunt magnam facere illum reiciendis suscipit ad nisi dicta totam. Laboriosam facere quasi assumenda."
+  },
+  {
+    title: "First slide label",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam reprehenderit, nostrum deserunt magnam facere illum reiciendis suscipit ad nisi dicta totam. Laboriosam facere quasi assumenda."
+  }
+];
