6d50d4213e447136972267e5a7b45fce3f2c14a2
Vladislav <vlad.argus@gmail.com>
2023-06-24 17:58:10 +0300

    Fixed favorites page

--- src/app/components/page/favoritesPage.jsx
+++ src/app/components/page/favoritesPage.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getTeam } from "../../store/team";
+import { getTeam, getTeamLoadingStatus } from "../../store/team";
 import UserCard from "../ui/userCard";
 import localStorageService from "../../services/localStorage.service";
 import Btn from "../common/btn";
 
 const FavoritesPage = () => {
   const team = useSelector(getTeam());
-  if (!team) return "Loading...";
+  const isLoading = useSelector(getTeamLoadingStatus());
 
-  const filteredTeam = team.filter((member) =>
-    localStorageService.getFavorites().includes(member.nick)
-  );
+  const [isUpdated, setUpdated] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const filteredTeam = team.filter((member) =>
+        localStorageService.getFavorites().includes(member.nick)
+      );
+      setFavorites(filteredTeam);
+      setUpdated(false);
+    }
+  }, [isUpdated]);
 
   const handleRemoveFavorite = (nick) => {
     localStorageService.removeFavorite(nick);
+    setUpdated(true);
   };
+
+  if (isLoading) return "Loading...";
 
   return (
     <div className="d-flex flex-column h-100">
@@ -23,26 +35,32 @@ const FavoritesPage = () => {
         <h1 className="text-center text-light">Избранные</h1>
       </header>
       <main className="bg-dark flex-fill">
-        <ul className="cards-container">
-          {filteredTeam.map((member) => (
-            <li key={member.nick} className="m-4">
-              <UserCard
-                nick={member.nick}
-                label={member.name}
-                age={member.age}
-                src={member.photo}
-              />
-
-              <div className="mt-3 d-flex justify-content-center">
-                <Btn
-                  color="danger"
-                  onClick={() => handleRemoveFavorite(member.nick)}
-                  label="Убрать из избранных"
+        {favorites.length > 0 ? (
+          <ul className="cards-container">
+            {favorites.map((member) => (
+              <li key={member.nick} className="m-4">
+                <UserCard
+                  nick={member.nick}
+                  label={member.name}
+                  age={member.age}
+                  src={member.photo}
                 />
-              </div>
-            </li>
-          ))}
-        </ul>
+
+                <div className="mt-3 d-flex justify-content-center">
+                  <Btn
+                    color="danger"
+                    onClick={() => handleRemoveFavorite(member.nick)}
+                    label="Убрать из избранных"
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="h-100 d-flex justify-content-center align-items-center">
+            <h3 className="text-light">Нет избранных</h3>
+          </div>
+        )}
       </main>
     </div>
   );
